a848170b3b7e134fc79f4a3b0e8ae3709014f6e0
Sandrin <Sandrin8181@mail.ru>
2016-08-28 14:28:54 +0300

    refactoring

--- src/main/java/LogParser.java
+++ src/main/java/LogParser.java
@@ -3,34 +3,32 @@ import org.openqa.selenium.logging.LogEntry;
 import java.util.Date;
 import java.util.List;
 
-public class LogParser {
+class LogParser {
 
     private List<LogEntry> logEntryList;
     private Date startTime;
 
+    //Конструктор принимает массив лога и дату с кототорой начать поиск
     LogParser(List<LogEntry> log, Date date){
         logEntryList = log;
         startTime = date;
     }
 
-    //поиск строки по логам
+    //Поиск строки по логам
     public boolean FindStringInLog(String findString ){
-        //итератор для перебора массива
-        int i = 0;
+
         //перебор массива
         for(LogEntry l :  logEntryList ){
             //фильтрую по тегу ReportManager и по времени тапа
-            if(logEntryList.get(i).getTimestamp() > startTime.getTime()
-                    && logEntryList.get(i).getMessage().contains("D/[Ya:ReportManager]")){
+            if(l.getTimestamp() > startTime.getTime()
+                    && l.getMessage().contains("D/[Ya:ReportManager]")){
                 //ищу строку
-                if(logEntryList.get(i).getMessage().contains(findString)){
-                    //совпадение найдено
+                if(l.getMessage().contains(findString)){
+                    //совпадение найдено, отдаём на выходе true
                     return true;
                 }
             }
-            //не нашли идём на сл.круг
-            i++;
-        }
+         }
         //не нашли
         return false;
     }
